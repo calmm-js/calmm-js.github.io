b6d2a5f8dd826f6cd2990cac87f5be2cf4adc69c
Vesa Karvonen <vesa.a.j.k@gmail.com>
2017-04-06 15:55:19 +0300

    A client only site

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React    from "karet"
+import {render} from "react-dom"
+
+const App = () =>
+  <div>
+    <h1>Calmm.js</h1>
+    <p>
+      Sorry! There is nothing here—<em>yet</em>. Click <a
+      href="https://github.com/calmm-js">here</a> to go to the GitHub org page.
+    </p>
+  </div>
+
+render(<App/>, document.getElementById("app"))
